b5f58972012dadba4e122abc59ec7a81075c201f
brookelove <sblove@bu.edu>
2023-09-13 12:35:00 -0400

    adding models initally

---
+++ models/Order.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class Order extends Model {}
+
+Order.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    total_price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product_tag",
+  }
+);
+
+module.exports = Order;
---
+++ models/index.js
@@ -0,0 +1,77 @@
+const Category = require("./Category");
+const Comment = require("./Comment");
+const Product = require("./Product");
+const Order = require("./Order");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+const User = require("./User");
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  // as: 'product_tags',
+  foreignKey: "product_id",
+});
+
+// Products have many Comments
+Product.hasMany(Comment, {
+  foreignKey: "productId",
+  onDelete: "CASCADE",
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  // as: 'product_tags',
+  foreignKey: "tag_id",
+});
+// Comments belondsToMany User
+Comment.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+// User have many Products
+User.hasMany(Product, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+// User have many Products
+User.hasMany(Order, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+// User belongToMany Products (through ProductTag)
+User.belongsToMany(Product, {
+  through: Order,
+  // as: 'product_tags',
+  foreignKey: "user_id",
+});
+// User belongToMany Products (through ProductTag)
+Product.belongsToMany(User, {
+  through: Order,
+  // as: 'product_tags',
+  foreignKey: "product_id",
+});
+
+module.exports = {
+  Category,
+  Comment,
+  Product,
+  Order,
+  Tag,
+  ProductTag,
+  User,
+};
