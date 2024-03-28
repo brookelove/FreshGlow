a09965c30feff5116468abc4b72b3b12a8e80653
brookelove <sblove@bu.edu>
2024-03-28 16:32:47 -0400

    allowing application to have access to the frontend

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://freshglow.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
